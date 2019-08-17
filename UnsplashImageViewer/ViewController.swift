e551f36266ab5b65082ae1ce9327fb350af01ff7
Микола Омельянов <mykola.omelyanov@gmail.com>
2019-08-17 19:57:06 +0300

    Initial Commit

---
+++ UnsplashImageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnsplashImageViewer
+//
+//  Created by Nikolas Omelianov on 8/17/19.
+//  Copyright © 2019 Nikolas Omelianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
